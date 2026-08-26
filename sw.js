39681ad0d32677ca4e2d36c68d4b33cffcda2d52
AnnaKarasowska <ak.eltrox@gmail.com>
2026-08-26 09:01:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,44 @@
-const CACHE='eltrox-rks-v14-12-shell';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.origin!==self.location.origin)return;
-  if(e.request.mode==='navigate'){
-    e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
+const CACHE='eltrox-rks-v15-0-shell';
+const ASSETS=['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',event=>{
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request)
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(c=>c.put('./index.html',copy));
+          return response;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
     return;
   }
-  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(e.request.method==='GET'){const x=r.clone();caches.open(CACHE).then(k=>k.put(e.request,x))}return r})));
+
+  event.respondWith(
+    caches.match(event.request).then(cached=>
+      cached || fetch(event.request).then(response=>{
+        if(event.request.method==='GET'){
+          const copy=response.clone();
+          caches.open(CACHE).then(c=>c.put(event.request,copy));
+        }
+        return response;
+      })
+    )
+  );
 });
