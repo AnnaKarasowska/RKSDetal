bf767ec72c91b50f4c13e9753e8662be36873969
AnnaKarasowska <ak.eltrox@gmail.com>
2026-08-26 10:49:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='eltrox-rks-v15-7-shell';
+const CACHE='eltrox-rks-v15-8-shell';
 const ASSETS=['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
